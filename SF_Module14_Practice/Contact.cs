c2a66f97bea0090cf8d0bcff8e376a2f17d486f5
oraclethirteen <oracle.thirteen.o@gmail.com>
2022-06-29 22:31:12 +0300

    Updated project

---
+++ SF_Module14_Practice/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace SF_Module14_Practice
+{
+    public class Contact
+    {
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+
+        public Contact(string name, string lastName, long phoneNumber, String email)
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
--- SF_Module14_Practice/Program.cs
+++ SF_Module14_Practice/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace SF_Module14_Practice
 {
@@ -6,7 +9,21 @@ namespace SF_Module14_Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var phoneBook = new List<Contact>();
+
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            var sortedPhoneBook = phoneBook
+                .OrderBy(p => p.Name)
+                .ThenBy(p => p.LastName);
+
+            foreach (var contact in phoneBook)
+                Console.WriteLine(contact.Name + " " + contact.LastName + ", тел. " + contact.PhoneNumber);
         }
     }
 }
